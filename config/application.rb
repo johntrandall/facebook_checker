da0fd722a7aab838285dd33c6c718fe8f22a550f
John Randall <john@johnrandall.com>
2021-03-04 09:53:10 -0500

    hail-mary google chrome shim

--- config/application.rb
+++ config/application.rb
@@ -22,9 +22,9 @@ module FacebookChecker
     # point to the heroku Chrome from heroku-buildpack-google-chrome
     #
 
-    # if ENV['HEROKU'].present?
-    #   Selenium::WebDriver::Chrome.path = ENV.fetch('GOOGLE_CHROME_SHIM')
-    # end
+    if ENV['HEROKU'].present?
+      Selenium::WebDriver::Chrome.path = ENV.fetch('GOOGLE_CHROME_SHIM')
+    end
     #
     # Capybara.register_driver :chrome do |app|
     #   Capybara::Selenium::Driver.new(app, browser: :chrome)
